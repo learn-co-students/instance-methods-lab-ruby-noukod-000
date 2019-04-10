e780d857cfebef2207fffe36b1a7d120abfdcd0c
James Declerus <james.declerus@esih.edu>
2019-04-10 14:34:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+ #Defining class dog
+class Dog
+#method bark
+  def bark
+    puts "Woof!"
+  end
+#method sitting
+def sit
+  puts "The Dog is sitting"
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+
+
+#definition of class person
+class Person
+#definition of method talk
+def talk
+  puts "Hello World!"
+
+end
+
+#definition of method walk
+
+def walk
+
+  puts "The Person is walking"
+
+end
+end
